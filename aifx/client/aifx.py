fad0b4f64bb3a2105966506263e3b213de66f6aa
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2026-05-02 23:17:30 -0400

    Release v0.15.5

--- aifx/client/aifx.py
+++ aifx/client/aifx.py
@@ -13,27 +13,25 @@ from aifx.util.utils import get_his_data_filename, get_instruments_data_filename
 from aifx.constants.DPairs import DPair as PAIR
 from aifx.constants.DFrequency import DFrequency as FREQ
 
-class AiFx(QtWidgets.QWidget):
+
+class aifx(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
         self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
 
         self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
+        self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
 
         self.layout = QtWidgets.QVBoxLayout(self)
         self.view = QWebEngineView()
         self.layout.addWidget(self.view)
-        
+
         self.button.clicked.connect(self.magic)
 
     @QtCore.Slot()
     def magic(self):
-        fig = go.Figure(data=[
-            go.Scatter(y=[random.randint(0, 10) for _ in range(20)])
-        ])
+        fig = go.Figure(data=[go.Scatter(y=[random.randint(0, 10) for _ in range(20)])])
         html = fig.to_html(include_plotlyjs="cdn")
         self.view.setHtml(html)
 
@@ -41,7 +39,7 @@ class AiFx(QtWidgets.QWidget):
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
-    widget = AiFx()
+    widget = aifx()
     widget.resize(1400, 600)
     widget.show()
 
