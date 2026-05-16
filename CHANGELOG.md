5dcd7cb030ab8ff01e08afc88a6cab0a00b263e8
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2026-05-16 12:04:09 -0400

    v0.15.19 - DevOps Plumbing

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [Release 0.15.19] - 2026-05-16 12:04
+
+
 **Tested Release Script**
 
 ---
--- aifx/constants/DDef.py
+++ aifx/constants/DDef.py
@@ -14,6 +14,6 @@ from aifx.constants.DLogging import DAiFxLog
 
 class DDef:
     DEFAULT_LOG_LEVEL: Final[str] = DAiFxLog.DEBUG
-    VERSION: Final[str] = "0.15.18"
+    VERSION: Final[str] = "0.15.19"
     MAX_PLOTLY_CANDLES: Final[int] = 50
     RECENT_CANDLE_MAX: Final[int] = 6
