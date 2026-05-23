51819e984beda4198a10b8efbfd0d73a0add787a
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2026-05-23 09:28:50 -0400

    v0.16.3 - Website polish

--- aifx/constants/DDef.py
+++ aifx/constants/DDef.py
@@ -15,7 +15,7 @@ from aifx.constants.DLogging import DAiFxLog
 
 class DDef:
     DEFAULT_LOG_LEVEL: Final[str] = DAiFxLog.DEBUG
-    VERSION: Final[str] = "0.16.2"
+    VERSION: Final[str] = "0.16.3"
     MAX_PLOTLY_CANDLES: Final[int] = 40
     RECENT_CANDLE_MAX: Final[int] = 6
     TICK_LENGTH_MS: Final[int] = 250
