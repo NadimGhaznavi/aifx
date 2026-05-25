285cad69b8c42b7dfdfcee04cc84748055bdf46e
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2026-05-24 21:22:39 -0400

    Feat: Updated Qt Client screenshot

--- aifx/client/ui_form.py
+++ aifx/client/ui_form.py
@@ -62,8 +62,13 @@ class Ui_Widget(object):
 
         self.verticalLayout.addLayout(self.vl_instrument)
 
-        self.hl_latency_plots = QHBoxLayout()
-        self.hl_latency_plots.setObjectName(u"hl_latency_plots")
+        self.hl_latency = QHBoxLayout()
+        self.hl_latency.setObjectName(u"hl_latency")
+        self.wgt_plot_brain_latency = QWidget(Widget)
+        self.wgt_plot_brain_latency.setObjectName(u"wgt_plot_brain_latency")
+
+        self.hl_latency.addWidget(self.wgt_plot_brain_latency)
+
         self.wgt_plot_broker_latency = QWidget(Widget)
         self.wgt_plot_broker_latency.setObjectName(u"wgt_plot_broker_latency")
         sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
@@ -73,7 +78,7 @@ class Ui_Widget(object):
         self.wgt_plot_broker_latency.setSizePolicy(sizePolicy1)
         self.wgt_plot_broker_latency.setMinimumSize(QSize(0, 150))
 
-        self.hl_latency_plots.addWidget(self.wgt_plot_broker_latency)
+        self.hl_latency.addWidget(self.wgt_plot_broker_latency)
 
         self.wgt_plot_oanda_latency = QWidget(Widget)
         self.wgt_plot_oanda_latency.setObjectName(u"wgt_plot_oanda_latency")
@@ -81,86 +86,30 @@ class Ui_Widget(object):
         self.wgt_plot_oanda_latency.setSizePolicy(sizePolicy1)
         self.wgt_plot_oanda_latency.setMinimumSize(QSize(0, 150))
 
-        self.hl_latency_plots.addWidget(self.wgt_plot_oanda_latency)
+        self.hl_latency.addWidget(self.wgt_plot_oanda_latency)
 
 
-        self.verticalLayout.addLayout(self.hl_latency_plots)
-
-        self.horizontalLayout_2 = QHBoxLayout()
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.hl_broker_status = QHBoxLayout()
-        self.hl_broker_status.setObjectName(u"hl_broker_status")
-        self.lbl_broker = QLabel(Widget)
-        self.lbl_broker.setObjectName(u"lbl_broker")
-        font2 = QFont()
-        font2.setFamilies([u"DejaVu Sans Mono"])
-        font2.setPointSize(16)
-        self.lbl_broker.setFont(font2)
-        self.lbl_broker.setAlignment(Qt.AlignBottom|Qt.AlignLeading|Qt.AlignLeft)
-
-        self.hl_broker_status.addWidget(self.lbl_broker)
-
-        self.lbl_broker_status = QLabel(Widget)
-        self.lbl_broker_status.setObjectName(u"lbl_broker_status")
-        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
-        sizePolicy2.setHorizontalStretch(0)
-        sizePolicy2.setVerticalStretch(0)
-        sizePolicy2.setHeightForWidth(self.lbl_broker_status.sizePolicy().hasHeightForWidth())
-        self.lbl_broker_status.setSizePolicy(sizePolicy2)
-        self.lbl_broker_status.setFont(font2)
-        self.lbl_broker_status.setAlignment(Qt.AlignBottom|Qt.AlignRight|Qt.AlignTrailing)
-
-        self.hl_broker_status.addWidget(self.lbl_broker_status)
-
-
-        self.horizontalLayout_2.addLayout(self.hl_broker_status)
-
-        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.horizontalLayout_2.addItem(self.horizontalSpacer_2)
-
-        self.hl_oanda_status = QHBoxLayout()
-        self.hl_oanda_status.setObjectName(u"hl_oanda_status")
-        self.lbl_oanda = QLabel(Widget)
-        self.lbl_oanda.setObjectName(u"lbl_oanda")
-        self.lbl_oanda.setFont(font2)
-        self.lbl_oanda.setAlignment(Qt.AlignBottom|Qt.AlignLeading|Qt.AlignLeft)
-
-        self.hl_oanda_status.addWidget(self.lbl_oanda)
-
-        self.lbl_oanda_status = QLabel(Widget)
-        self.lbl_oanda_status.setObjectName(u"lbl_oanda_status")
-        self.lbl_oanda_status.setFont(font2)
-        self.lbl_oanda_status.setAlignment(Qt.AlignBottom|Qt.AlignRight|Qt.AlignTrailing)
-
-        self.hl_oanda_status.addWidget(self.lbl_oanda_status)
-
-
-        self.horizontalLayout_2.addLayout(self.hl_oanda_status)
-
-        self.horizontalLayout_2.setStretch(1, 1)
-
-        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.verticalLayout.addLayout(self.hl_latency)
 
         self.hl_instrument = QHBoxLayout()
         self.hl_instrument.setObjectName(u"hl_instrument")
         self.lbl_instrument = QLabel(Widget)
         self.lbl_instrument.setObjectName(u"lbl_instrument")
-        sizePolicy3 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
-        sizePolicy3.setHorizontalStretch(0)
-        sizePolicy3.setVerticalStretch(0)
-        sizePolicy3.setHeightForWidth(self.lbl_instrument.sizePolicy().hasHeightForWidth())
-        self.lbl_instrument.setSizePolicy(sizePolicy3)
+        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.lbl_instrument.sizePolicy().hasHeightForWidth())
+        self.lbl_instrument.setSizePolicy(sizePolicy2)
 
         self.hl_instrument.addWidget(self.lbl_instrument)
 
         self.cb_instrument = QComboBox(Widget)
         self.cb_instrument.setObjectName(u"cb_instrument")
-        sizePolicy4 = QSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Fixed)
-        sizePolicy4.setHorizontalStretch(0)
-        sizePolicy4.setVerticalStretch(0)
-        sizePolicy4.setHeightForWidth(self.cb_instrument.sizePolicy().hasHeightForWidth())
-        self.cb_instrument.setSizePolicy(sizePolicy4)
+        sizePolicy3 = QSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Fixed)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(self.cb_instrument.sizePolicy().hasHeightForWidth())
+        self.cb_instrument.setSizePolicy(sizePolicy3)
         self.cb_instrument.setMinimumSize(QSize(350, 0))
 
         self.hl_instrument.addWidget(self.cb_instrument)
@@ -182,11 +131,11 @@ class Ui_Widget(object):
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.btn_exit = QPushButton(Widget)
         self.btn_exit.setObjectName(u"btn_exit")
-        sizePolicy5 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
-        sizePolicy5.setHorizontalStretch(0)
-        sizePolicy5.setVerticalStretch(0)
-        sizePolicy5.setHeightForWidth(self.btn_exit.sizePolicy().hasHeightForWidth())
-        self.btn_exit.setSizePolicy(sizePolicy5)
+        sizePolicy4 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
+        sizePolicy4.setHorizontalStretch(0)
+        sizePolicy4.setVerticalStretch(0)
+        sizePolicy4.setHeightForWidth(self.btn_exit.sizePolicy().hasHeightForWidth())
+        self.btn_exit.setSizePolicy(sizePolicy4)
 
         self.horizontalLayout.addWidget(self.btn_exit)
 
@@ -207,10 +156,6 @@ class Ui_Widget(object):
     def retranslateUi(self, Widget):
         Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Widget", None))
         self.lbl_current_pair.setText(QCoreApplication.translate("Widget", u"<html><head/><body><p><span style=\" font-size:22pt; color:#00aa00;\">Currency Pair</span></p></body></html>", None))
-        self.lbl_broker.setText(QCoreApplication.translate("Widget", u"Broker", None))
-        self.lbl_broker_status.setText(QCoreApplication.translate("Widget", u"<html><head/><body><p><span style=\" font-weight:700; color:#ff7800;\">Uninitialized</span></p></body></html>", None))
-        self.lbl_oanda.setText(QCoreApplication.translate("Widget", u"Oanda", None))
-        self.lbl_oanda_status.setText(QCoreApplication.translate("Widget", u"<html><head/><body><p><span style=\" font-weight:700; color:#ff7800;\">Uninitialized</span></p></body></html>", None))
         self.lbl_instrument.setText(QCoreApplication.translate("Widget", u"Instrument:", None))
         self.btn_load.setText(QCoreApplication.translate("Widget", u"Load", None))
         self.btn_exit.setText(QCoreApplication.translate("Widget", u"Exit", None))
