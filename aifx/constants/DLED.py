90913b50a676b7f0806951fdfc6582e496ecfea1
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2026-05-30 21:52:56 -0400

    Feat: LED lights

---
+++ aifx/constants/DLED.py
@@ -0,0 +1,17 @@
+# aifx/constants/DLED.py
+#
+#    AI FX
+#    Author: Nadim-Daniel Ghaznavi
+#    Copyright: (c) 2026 Nadim-Daniel Ghaznavi
+#    GitHub: https://github.com/NadimGhaznavi/aifx
+#    Website: https://aifx.osoyalce.com
+#    License: GPL 3.0
+
+from typing import Final
+
+
+class DLED:
+    BLUE: Final[str] = "🔵"
+    GREEN: Final[str] = "🟢"
+    RED: Final[str] = "🔴"
+    YELLOW: Final[str] = "🟡"
